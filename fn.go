dc82b0606aee7c6ed86c1f2e99de486a2199d8bf
Markku Rossi <mtr@iki.fi>
2020-01-08 11:27:07 -0500

    Added dependencies.

--- fn.go
+++ fn.go
@@ -5,10 +5,27 @@
 package cloudfunctionstest
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/compute/v1"
 )
 
+var projectID string
+
+func init() {
+	ctx := context.Background()
+
+	credentials, err := google.FindDefaultCredentials(ctx, compute.ComputeScope)
+	if err != nil {
+		fmt.Printf("FindDefaultCredentials failed: %s\n", err)
+	}
+
+	projectID = credentials.ProjectID
+}
+
 func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!\n")
+	fmt.Fprintf(w, "Hello, world!\nProjectID=%s\n", projectID)
 }
